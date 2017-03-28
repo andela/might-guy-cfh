8be7608971ea6ef069030fb430480e72594cfeaf
andela-aonifade <anuoluwapo.onifade@andela.com>
2017-03-28 01:59:07 +0100

    feature:generates jwt for user on successful login user receives jwt on successful login and user uses jwt to make subsequent request Finishes #141409253

--- config/api/routes.js
+++ config/api/routes.js
@@ -12,6 +12,7 @@ module.exports = (app) => {
       User.findOne({
         email: req.body.email
       }).exec((err, existingUser) => {
+        if (err) throw err;
         if (!existingUser) {
           const user = new User(req.body);
           // Switch the user's avatar index to an actual avatar url
@@ -23,7 +24,7 @@ module.exports = (app) => {
                 message: `unknown error : ${err.errors}` });
             }
             req.logIn(user, (err) => {
-              if (err) return next(err);
+              if (err) return next (err);
               const token = jwt.sign(user, config.secret, {
                 expiresIn: 10080 // in seconds
               });
@@ -36,6 +37,40 @@ module.exports = (app) => {
       });
     } else {
       res.json({ success: false, message: 'You must enter all field' });
+    }
+  });
+
+  // Create signin api
+  app.post('/api/auth/signin', (req, res) => {
+    if (!req.body.email || !req.body.password) {
+      res.json({ success: false,
+        message: 'You need to enter username and password' });
+    } else {
+      User.findOne({
+        email: req.body.email
+      }, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+          res.json({
+            success: false,
+            message: 'User does not exist'
+          });
+        }
+        const isMatch = user.authenticate(req.body.password);
+        //  console.log('isMatch', isMatch);
+        if (isMatch === true) {
+          req.logIn(user, (err) => {
+            if (err) return next(err);
+            const token = jwt.sign(user, config.secret, {
+              expiresIn: 10080 // in seconds
+            });
+            res.json({ success: true, token: `JWT ${token}` });
+          });
+        } else {
+          res.json({ success: false,
+            message: 'Unable to Login. Invalid credentials' });
+        }
+      });
     }
   });
 };
--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,37 +1,55 @@
+
 angular.module('mean.system')
-.controller('IndexController', ['$scope', '$http', 'Global', '$location', 'socket', 'game', 'AvatarService', 'LS', '$window', ($scope, $http, Global, $location, socket, game, AvatarService, LS, $window) => {
-  $scope.global = Global;
-  $scope.formData = {};
-  $scope.showDialog = false;
-  $scope.dialogMessage = '';
+.controller('IndexController', ['$scope', '$http', 'Global',
+  '$location', 'socket', 'game', 'AvatarService',
+  '$window',
+  ($scope, $http, Global, $location, socket, game,
+  AvatarService, $window) => {
+    $scope.global = Global;
+    $scope.formData = {};
 
-  $scope.playAsGuest = () => {
-    game.joinGame();
-    $location.path('/app');
-  };
+    $scope.playAsGuest = () => {
+      game.joinGame();
+      $location.path('/app');
+    };
 
-  $scope.showError = () => {
-    if ($location.search().error) {
-      return $location.search().error;
-    }
-    return false;
-  };
-  $scope.signUp = () => {
-    $http.post('/api/auth/signup', $scope.formData)
+    $scope.showError = () => {
+      if ($location.search().error) {
+        return $location.search().error;
+      }
+      return false;
+    };
+    $scope.signUp = () => {
+      $http.post('/api/auth/signup', $scope.formData)
       .success((data) => {
         if (data.success === true) {
           $window.localStorage.setItem('user-token', data.token);
           $window.location.href = '/';
         } else {
-          // Show Login Failure dialogMessage
-          console.log(data.message);
+          $scope.showMessage = data.message;
         }
+      }).error((error, status) => {
+        $scope.showMessage = `${status} : ${error}`;
       });
-  };
+    };
 
-  $scope.avatars = [];
-  AvatarService.getAvatars()
+    $scope.signIn = () => {
+      $http.post('/api/auth/signin', $scope.formData)
+        .success((data) => {
+          if (data.success === true) {
+            $window.localStorage.setItem('user-token', data.token);
+            $window.location.href = '/';
+          } else {
+            $scope.showMessage = data.message;
+          }
+        }).error((error, status) => {
+          $scope.showMessage = `${status} : ${error}`;
+        });
+    };
+
+    $scope.avatars = [];
+    AvatarService.getAvatars()
       .then((data) => {
         $scope.avatars = data;
       });
-}]);
+  }]);
