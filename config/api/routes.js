d2a12273324ae8211a62453c6610a990cb5761f8
andela-aonifade <anuoluwapo.onifade@andela.com>
2017-03-28 01:19:35 +0100

    feature:generates jwt for user on successful signup user receives jwt on successful signup and user uses jwt to make subsequent request Finishes #141409255

---
+++ config/api/routes.js
@@ -0,0 +1,41 @@
+//  const async = require('async');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose'),
+  User = mongoose.model('User');
+const avatars = require('../../app/controllers/avatars').all();
+const config = require('../config');
+
+module.exports = (app) => {
+    //  api Routes
+  app.post('/api/auth/signup', (req, res) => {
+    if (req.body.name && req.body.password && req.body.email) {
+      User.findOne({
+        email: req.body.email
+      }).exec((err, existingUser) => {
+        if (!existingUser) {
+          const user = new User(req.body);
+          // Switch the user's avatar index to an actual avatar url
+          user.avatar = avatars[user.avatar];
+          user.provider = 'jwt';
+          user.save((err) => {
+            if (err) {
+              res.json({ success: false,
+                message: `unknown error : ${err.errors}` });
+            }
+            req.logIn(user, (err) => {
+              if (err) return next(err);
+              const token = jwt.sign(user, config.secret, {
+                expiresIn: 10080 // in seconds
+              });
+              res.json({ success: true, token: `JWT ${token}` });
+            });
+          });
+        } else {
+          res.json({ success: false, message: 'User already exist' });
+        }
+      });
+    } else {
+      res.json({ success: false, message: 'You must enter all field' });
+    }
+  });
+};
--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,24 +1,61 @@
+
 angular.module('mean.system')
-.controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, Global, $location, socket, game, AvatarService) {
-    $scope.global = Global;
+.controller('IndexController',
+  [
+    '$scope',
+    '$http',
+    'Global',
+    '$location',
+    'socket',
+    'game',
+    'AvatarService',
+    '$window',
+    ($scope, $http, Global, $location, socket, game,
+  AvatarService, $window) => {
+      $scope.global = Global;
+      $scope.formData = {};
+      $scope.showDialog = false;
+      $scope.dialogMessage = '';
 
-    $scope.playAsGuest = function() {
-      game.joinGame();
-      $location.path('/app');
-    };
+      $scope.playAsGuest = () => {
+        game.joinGame();
+        $location.path('/app');
+      };
 
-    $scope.showError = function() {
-      if ($location.search().error) {
-        return $location.search().error;
-      } else {
+      $scope.showError = () => {
+        if ($location.search().error) {
+          return $location.search().error;
+        }
         return false;
-      }
-    };
+      };
 
-    $scope.avatars = [];
-    AvatarService.getAvatars()
-      .then(function(data) {
+      $scope.signUp = () => {
+        $http.post('/api/auth/signup', $scope.formData)
+      .success((data) => {
+        if (data.success === true) {
+          $window.localStorage.setItem('user-token', data.token);
+          $window.location.href = '/';
+        } else {
+          // Show Login Failure Message
+        }
+      });
+      };
+
+      $scope.signIn = () => {
+        $http.post('/api/auth/signin', $scope.formData)
+        .success((data) => {
+          if (data.success === false) {
+            $window.localStorage.setItem('user-token', data.token);
+            $window.location.href = '/';
+          } else {
+              // Failure Message here
+          }
+        });
+      };
+
+      $scope.avatars = [];
+      AvatarService.getAvatars()
+      .then((data) => {
         $scope.avatars = data;
       });
-
-}]);
+    }]);
