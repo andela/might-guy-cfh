11ec8c216a8a2f5c4a099f588bd32f343207f2a2
Ufedojo Daniel Opaluwa <ufedojo.opaluwa@andela.com>
2017-03-30 18:36:03 +0100

    Revert "#141409253 && #141409255 Users should receive a JWT on successful signin and signup"

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,55 +1,24 @@
-
 angular.module('mean.system')
-.controller('IndexController', ['$scope', '$http', 'Global',
-  '$location', 'socket', 'game', 'AvatarService',
-  '$window',
-  ($scope, $http, Global, $location, socket, game,
-  AvatarService, $window) => {
+.controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, Global, $location, socket, game, AvatarService) {
     $scope.global = Global;
-    $scope.formData = {};
 
-    $scope.playAsGuest = () => {
+    $scope.playAsGuest = function() {
       game.joinGame();
       $location.path('/app');
     };
 
-    $scope.showError = () => {
+    $scope.showError = function() {
       if ($location.search().error) {
         return $location.search().error;
+      } else {
+        return false;
       }
-      return false;
-    };
-    $scope.signUp = () => {
-      $http.post('/api/auth/signup', $scope.formData)
-      .success((data) => {
-        if (data.success === true) {
-          $window.localStorage.setItem('user-token', data.token);
-          $window.location.href = '/';
-        } else {
-          $scope.showMessage = data.message;
-        }
-      }).error((error, status) => {
-        $scope.showMessage = `${status} : ${error}`;
-      });
-    };
-
-    $scope.signIn = () => {
-      $http.post('/api/auth/signin', $scope.formData)
-        .success((data) => {
-          if (data.success === true) {
-            $window.localStorage.setItem('user-token', data.token);
-            $window.location.href = '/';
-          } else {
-            $scope.showMessage = data.message;
-          }
-        }).error((error, status) => {
-          $scope.showMessage = `${status} : ${error}`;
-        });
     };
 
     $scope.avatars = [];
     AvatarService.getAvatars()
-      .then((data) => {
+      .then(function(data) {
         $scope.avatars = data;
       });
-  }]);
+
+}]);
--- server.js
+++ server.js
@@ -2,73 +2,69 @@
  * Module dependencies.
  */
 require('dotenv').config();
-const express = require('express'),
-  fs = require('fs'),
-  passport = require('passport'),
-  logger = require('mean-logger'),
-  io = require('socket.io');
+var express = require('express'),
+    fs = require('fs'),
+    passport = require('passport'),
+    logger = require('mean-logger'),
+    io = require('socket.io');
 
 /**
  * Main application entry file.
  * Please note that the order of loading is important.
  */
 
-// Load configurations
-// if test env, load example file
-const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-  config = require('./config/config'),
-  auth = require('./config/middlewares/authorization'),
-  mongoose = require('mongoose');
+//Load configurations
+//if test env, load example file
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
+    config = require('./config/config'),
+    auth = require('./config/middlewares/authorization'),
+    mongoose = require('mongoose');
 
-// Bootstrap db connection
-mongoose.connect(config.db);
+//Bootstrap db connection
+var db = mongoose.connect(config.db);
 
-// Bootstrap models
-const models_path = `${__dirname}/app/models`;
-const walk = (path) => {
-  fs.readdirSync(path).forEach((file) => {
-    const newPath = `${path}/${file}`;
-    const stat = fs.statSync(newPath);
-    if (stat.isFile()) {
-      if (/(.*)\.(js|coffee)/.test(file)) {
-        require(newPath);
-      }
-    } else if (stat.isDirectory()) {
-      walk(newPath);
-    }
-  });
+//Bootstrap models
+var models_path = __dirname + '/app/models';
+var walk = function(path) {
+    fs.readdirSync(path).forEach(function(file) {
+        var newPath = path + '/' + file;
+        var stat = fs.statSync(newPath);
+        if (stat.isFile()) {
+            if (/(.*)\.(js|coffee)/.test(file)) {
+                require(newPath);
+            }
+        } else if (stat.isDirectory()) {
+            walk(newPath);
+        }
+    });
 };
 walk(models_path);
 
-// bootstrap passport config
+//bootstrap passport config
 require('./config/passport')(passport);
 
-const app = express();
+var app = express();
 
-app.use((req, res, next) => {
-  next();
+app.use(function(req, res, next){
+    next();
 });
 
-// express settings
+//express settings
 require('./config/express')(app, passport, mongoose);
 
-// Bootstrap routes
+//Bootstrap routes
 require('./config/routes')(app, passport, auth);
 
-// API routes
-require('./config/api/routes')(app, passport, auth);
-
-// Start the app by listening on <port>
-const port = config.port;
-const server = app.listen(port);
-const ioObj = io.listen(server, { log: false });
-// game logic handled here
+//Start the app by listening on <port>
+var port = config.port;
+var server = app.listen(port);
+var ioObj = io.listen(server, { log: false });
+//game logic handled here
 require('./config/socket/socket')(ioObj);
+console.log('Express app started on port ' + port);
 
-console.log(`Express app started on port ${port}`);
-
-// Initializing logger
+//Initializing logger
 logger.init(app, passport, mongoose);
 
-// expose app
+//expose app
 exports = module.exports = app;
